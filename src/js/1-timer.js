1b5a9c58dac01dc78c6a92d9468c30baf990c982
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-03-29 23:10:00 +0100

    upd

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,14 +1,77 @@
 import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-const input = document.querySelector('input[type="text"]');
-console.log(input);
+const startBtn = document.querySelector('button[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+const timer = document.querySelector('.timer');
+
+let userSelectedDate = Date.now();
+startBtn.disabled = false;
+let currentTime = Date.now();
+
+// iziToast.show({
+//   message: 'Please choose a date in the future',
+// });
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+
+    if (selectedDates[0] < userSelectedDate) {
+      iziToast.show({
+        message: 'Please choose a date in the future',
+        messageColor: '#FFFFF0',
+        backgroundColor: '#FF4500',
+        position: 'topRight',
+        progressBar: false,
+        close: false,
+      });
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
   },
 };
+
+flatpickr('input[type="text"]', options);
+
+// function updateTimer({ days, hours, mins, second })
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+startBtn.addEventListener('click', () => {
+  const delatTime = userSelectedDate - currentTime;
+  console.log(delatTime);
+  const timeCount = this.convertMs(delatTime);
+});
